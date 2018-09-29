efd2640ba3fe77a715b057bb9b4404a58437bf73
Tim Jensen <tim.l.jensen@gmail.com>
2018-09-29 16:07:39 -0500

    Added reducers for the rest of the state

--- __tests__/reducers.js
+++ __tests__/reducers.js
@@ -2,7 +2,7 @@ import reducers from '../src/reducers';
 import * as actions from '../src/actions';
 
 
-describe('serviceBaseURL reducers', () => {
+describe('serviceBaseURL reducer', () => {
   it('defaults to null', () => {
     const {serviceBaseURL} = reducers({}, {dont: 'care'});
 
@@ -13,5 +13,73 @@ describe('serviceBaseURL reducers', () => {
     const {serviceBaseURL} = reducers({}, actions.setServiceBaseURL('base/url'));
 
     expect(serviceBaseURL).toEqual('base/url');
+  });
+});
+
+describe('fetching reducer', () => {
+  it('defaults to false', () => {
+    const {fetching} = reducers({}, {dont: 'care'});
+
+    expect(fetching).toBeFalsy();
+  });
+
+  it('sets fetching to true when starting to fetch embed', () => {
+    const {fetching} = reducers({}, actions.startFetchEmbed('some/resource'));
+
+    expect(fetching).toBeTruthy();
+  });
+
+  it('sets fetching to false when fetching embed completes', () => {
+    const {fetching} = reducers({fetching: true}, actions.fetchEmbedCompleted('html'));
+
+    expect(fetching).toBeFalsy();
+  });
+
+  it('sets fetching to false when fetching embed fails', () => {
+    const {fetching} = reducers({fetching: true}, actions.fetchEmbedCompleted('html'));
+
+    expect(fetching).toBeFalsy();
+  });
+});
+
+describe('embedHTML reducer', () => {
+  it('defaults to empty string', () => {
+    const {embedHTML} = reducers({}, {dont: 'care'});
+
+    expect(embedHTML).toEqual('');
+  });
+
+  it('sets embedHTML when fetching embed completes', () => {
+    const {embedHTML} = reducers({}, actions.fetchEmbedCompleted('<some>html</some>'));
+
+    expect(embedHTML).toEqual('<some>html</some>');
+  });
+
+  it('sets embedHTML to empty string when fetching embed fails', () => {
+    const {embedHTML} = reducers(
+      {embedHTML: 'not empty'},
+      actions.fetchEmbedFailed('some error'));
+
+    expect(embedHTML).toEqual('');
+  });
+});
+
+describe('fetchError reducer', () => {
+  it('defaults to null', () => {
+    const {fetchError} = reducers({}, {dont: 'care'});
+
+    expect(fetchError).toBeNull();
+  });
+
+  it('sets fetchError to null when fetching embed completes', () => {
+    const {fetchError} = reducers({}, actions.fetchEmbedCompleted('some html'));
+
+    expect(fetchError).toBeNull();
+  });
+
+  it('sets fetchError when fetching embed fails', () => {
+    const {fetchError} = reducers({}, actions.fetchEmbedFailed('some error'));
+
+    expect(fetchError).toEqual('some error');
   });
 });
--- src/reducers.js
+++ src/reducers.js
@@ -12,6 +12,44 @@ function serviceBaseURL(state = null, action) {
   }
 }
 
+function fetching(state = false, action) {
+  switch (action.type) {
+    case types.START_FETCH_EMBED:
+      return true;
+    case types.FETCH_EMBED_COMPLETED:
+    case types.FETCH_EMBED_FAILED:
+      return false;
+    default:
+      return state;
+  }
+  return state;
+}
+
+function embedHTML(state = '', action) {
+  switch (action.type) {
+    case types.FETCH_EMBED_COMPLETED:
+      return action.content;
+    case types.FETCH_EMBED_FAILED:
+      return '';
+    default:
+      return state;
+  }
+}
+
+function fetchError(state = null, action) {
+  switch (action.type) {
+    case types.FETCH_EMBED_COMPLETED:
+      return null;
+    case types.FETCH_EMBED_FAILED:
+      return action.error;
+    default:
+      return state;
+  }
+}
+
 export default combineReducers({
-  serviceBaseURL
+  serviceBaseURL,
+  fetching,
+  embedHTML,
+  fetchError
 });
