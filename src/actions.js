46c9d470ad04de5c41f1a4754cb3c6de7ca9de35
Tim Jensen <tim.l.jensen@gmail.com>
2018-09-29 09:02:08 -0500

    Renamed type constants to be more descriptive

--- src/actions.js
+++ src/actions.js
@@ -3,27 +3,27 @@ import {call, put, select} from 'redux-saga/effects';
 import api from './api';
 
 
-const FETCH_START = 'FETCH_START';
-const FETCH_SUCCESS = 'FETCH_SUCCESS';
-const FETCH_FAILURE = 'FETCH_FAILURE';
+const FETCH_EMBED_STARTED = 'FETCH_EMBED_STARTED';
+const FETCH_EMBED_COMPLETED = 'FETCH_EMBED_COMPLETED';
+const FETCH_EMBED_FAILED = 'FETCH_EMBED_FAILED';
 
 
 export function start() {
   return {
-    type: FETCH_START
+    type: FETCH_EMBED_STARTED
   };
 }
 
 export function success(content) {
   return {
-    type: FETCH_SUCCESS,
+    type: FETCH_EMBED_COMPLETED,
     content
   };
 }
 
 export function failure(error) {
   return {
-    type: FETCH_FAILURE,
+    type: FETCH_EMBED_FAILED,
     error
   };
 }
